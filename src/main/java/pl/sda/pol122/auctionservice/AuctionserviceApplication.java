67253359bd260e5ebaa4b0eefe9ee73a44e331bc
Jakub Łanoszka <jakublanoszka@gmail.com>
2023-01-14 15:06:19 +0100

    spring installation

---
+++ src/main/java/pl/sda/pol122/auctionservice/AuctionserviceApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.pol122.auctionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/pol122/auctionservice/AuctionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.pol122.auctionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
