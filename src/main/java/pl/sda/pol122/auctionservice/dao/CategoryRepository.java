7e68f2e8a1584b93dc33dff5b433e5ee5d24c453
mkolodziejczyk92 <101639854+mkolodziejczyk92@users.noreply.github.com>
2023-01-15 13:11:01 +0100

    Add entities, category repository. (#2)

--- src/main/java/pl/sda/pol122/auctionservice/dao/CategoryRepository.java
+++ src/main/java/pl/sda/pol122/auctionservice/dao/CategoryRepository.java
@@ -1,4 +1,10 @@
 package pl.sda.pol122.auctionservice.dao;
 
-public interface CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.sda.pol122.auctionservice.entities.CategoryEntity;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer> {
+
 }
--- src/main/java/pl/sda/pol122/auctionservice/entities/ProductEntity.java
+++ src/main/java/pl/sda/pol122/auctionservice/entities/ProductEntity.java
@@ -1,4 +1,26 @@
 package pl.sda.pol122.auctionservice.entities;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
 public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "name", length = 135, nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private CategoryEntity categoryEntity;
+
+    @Column(name = "amount", nullable = false)
+    private Integer amount;
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
 }
