95b6b8a3d18d12ce17531aec8f6bed990cef0831
Savilus <105964835+Savilus@users.noreply.github.com>
2023-01-19 22:29:42 +0100

    Shopping cart html and css (#5)
    * html and css
    
    * html and css for cart

---
+++ src/main/java/pl/sda/pol122/auctionservice/model/Cart.java
@@ -0,0 +1,10 @@
+package pl.sda.pol122.auctionservice.model;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class Cart {
+}
