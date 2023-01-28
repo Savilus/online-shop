6fa3b642dc5c2a5e03b63ad2ef183c9664a2cf04
mkolodziejczyk92 <101639854+mkolodziejczyk92@users.noreply.github.com>
2023-01-28 13:33:50 +0100

    Add security filter chain (#34)

--- src/main/java/pl/sda/pol122/auctionservice/config/WebSecurityConfig.java
+++ src/main/java/pl/sda/pol122/auctionservice/config/WebSecurityConfig.java
@@ -3,10 +3,12 @@ package pl.sda.pol122.auctionservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -23,6 +25,13 @@ public class WebSecurityConfig{
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated());
+        return http.build();
     }
 
 }
