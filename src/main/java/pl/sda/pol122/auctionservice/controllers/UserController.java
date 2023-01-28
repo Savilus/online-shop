f1d8827ec2dcf3d1d4e91e5f8b09f726b0ad0944
MichLangner <108215862+MichLangner@users.noreply.github.com>
2023-01-28 16:03:02 +0100

    Update controllers. (#38)
    Co-authored-by: Michal <michal.langner@gmail.com>

--- src/main/java/pl/sda/pol122/auctionservice/controllers/UserController.java
+++ src/main/java/pl/sda/pol122/auctionservice/controllers/UserController.java
@@ -3,8 +3,10 @@ package pl.sda.pol122.auctionservice.controllers;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import pl.sda.pol122.auctionservice.model.Category;
 import pl.sda.pol122.auctionservice.model.Product;
 import pl.sda.pol122.auctionservice.model.User;
+import pl.sda.pol122.auctionservice.services.CategoriesService;
 import pl.sda.pol122.auctionservice.services.ProductService;
 import pl.sda.pol122.auctionservice.services.UserService;
 
@@ -15,6 +17,8 @@ public class UserController {
 
     private final UserService userService;
     private final ProductService productService;
+
+    private final CategoriesService categoriesService;
 
 
     @PostMapping("/addNewProduct")
@@ -45,6 +49,37 @@ public class UserController {
     @PatchMapping(path = "/account/editAccount")
     public String editUserProfile() {
         return "editUserProfile";
+    }
+
+    @PostMapping("/add-category")
+    public String addNewCategoryByAdmin(Category category) {
+        categoriesService.addNewCategory(category);
+        return "redirect:/categories";
+    }
+
+    @DeleteMapping("/users/{id}")
+    public String deleteUserByAdmin(@PathVariable String id) {
+        userService.deleteById(Integer.valueOf(id));
+        return "redirect:/users";
+    }
+
+
+    @PatchMapping("/updateUser/{id}")
+    public String updateAccountStatusByAdmin(@PathVariable String userId, boolean enabledFromInput) {
+        userService.saveAccountStatusByAdmin(Integer.valueOf(userId), enabledFromInput);
+        return "redirect:/users";
+    }
+
+    @PatchMapping("/updateProduct/{id}")
+    public String updateProductChangesByAdmin(Product product) {
+        productService.updateProductChanges(product);
+        return "redirect:/default";
+    }
+
+    @DeleteMapping("/product/{id}")
+    public String deleteProductByAdmin(@PathVariable String id) {
+        productService.deleteProductById(Integer.valueOf(id));
+        return "redirect:/default";
     }
 
 
