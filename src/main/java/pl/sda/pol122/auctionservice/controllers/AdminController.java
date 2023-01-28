4021a5ad946058bb1a63152d8eec3b3ec48830a9
Michal <michal.langner@gmail.com>
2023-01-28 15:53:09 +0100

    Update controllers.

--- src/main/java/pl/sda/pol122/auctionservice/controllers/AdminController.java
+++ src/main/java/pl/sda/pol122/auctionservice/controllers/AdminController.java
@@ -18,36 +18,7 @@ public class AdminController {
     private final UserService userService;
     private final ProductService productService;
 
-    @PostMapping("/add-category")
-    public String addNewCategoryByAdmin(Category category) {
-        categoriesService.addNewCategory(category);
-        return "redirect:/categories";
-    }
 
-    @DeleteMapping("/users/{id}")
-    public String deleteUserByAdmin(@PathVariable String id) {
-        userService.deleteById(Integer.valueOf(id));
-        return "redirect:/users";
-    }
-
-
-    @PatchMapping("/updateUser/{id}")
-    public String updateAccountStatusByAdmin(@PathVariable String userId, boolean enabledFromInput) {
-        userService.saveAccountStatusByAdmin(Integer.valueOf(userId), enabledFromInput);
-        return "redirect:/users";
-    }
-
-    @PatchMapping("/updateProduct/{id}")
-    public String updateProductChangesByAdmin(Product product) {
-        productService.updateProductChanges(product);
-        return "redirect:/default";
-    }
-
-    @DeleteMapping("/product/{id}")
-    public String deleteProductByAdmin(@PathVariable String id) {
-        productService.deleteProductById(Integer.valueOf(id));
-        return "redirect:/default";
-    }
 
 
 }
