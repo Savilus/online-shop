64cbce792bf2a4526aa509b306e5e6991e0dc69e
mkolodziejczyk92 <101639854+mkolodziejczyk92@users.noreply.github.com>
2023-01-29 14:24:22 +0100

    Add sign up validator (#43)

---
+++ src/main/java/pl/sda/pol122/auctionservice/controllers/validators/SignUpValidator.java
@@ -0,0 +1,58 @@
+package pl.sda.pol122.auctionservice.controllers.validators;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+import pl.sda.pol122.auctionservice.dao.UserRepository;
+import pl.sda.pol122.auctionservice.model.User;
+import pl.sda.pol122.auctionservice.services.UserService;
+
+import java.util.regex.Pattern;
+
+@Component
+public class SignUpValidator implements Validator {
+
+    private UserService userService;
+    private UserRepository userRepository;
+
+
+    public SignUpValidator(UserService userService, UserRepository userRepository) {
+        this.userService = userService;
+        this.userRepository = userRepository;
+    }
+
+    private static final String EXPECTED_PASSWORD_REGEX =
+            "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return User.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userName", "field.required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "field.required");
+        User user = (User) target;
+        Pattern patternMatcher = Pattern.compile(EXPECTED_PASSWORD_REGEX);
+        boolean isPasswordCorrect = patternMatcher.matcher(user.getPassword()).matches();
+
+        boolean isUserNameExist = userRepository.checkIfUsernameIsAvailable(user.getUserName()) == 1;
+
+        if (!isPasswordCorrect) {
+            errors.rejectValue(
+                    "password",
+                    "password.error",
+                    "The password should contain the lowercase letter, the uppercase letter," +
+                            " the number and the special character.");
+        }
+        if (isUserNameExist) {
+            errors.rejectValue(
+                    "userName",
+                    "userName.error",
+                    "The user name is exist.");
+        }
+    }
+}
--- src/main/java/pl/sda/pol122/auctionservice/dao/UserRepository.java
+++ src/main/java/pl/sda/pol122/auctionservice/dao/UserRepository.java
@@ -1,11 +1,17 @@
 package pl.sda.pol122.auctionservice.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.sda.pol122.auctionservice.entities.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
     UserEntity getUserEntityById(Integer id);
     void deleteById(Integer id);
+
+    @Query(value = "SELECT EXISTS(SELECT * FROM users WHERE username = :userName)",
+            nativeQuery = true )
+    Integer checkIfUsernameIsAvailable(@Param("userName") String userName);
 
 }
